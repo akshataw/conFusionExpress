e3552ee9213fca99f39b0db75a19d2af1f47b6da
akshata <wagheakshu@gmail.com>
2019-09-05 16:29:10 +0530

    add cookies

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
 // const url = "mongodb://localhost:27017/conFusion";
-const url = 'mongodb://akshatamw:akshata2607@ds153730.mlab.com:53730/amsdb'
+const url = 'mongodb://akshatamw:akshata2607@ds153730.mlab.com:53730/amsdb';
+// const url = process.env.MONGO_URI;
+// console.log("URL: " + url);
 
 const connect = mongoose.connect(url);
 
@@ -34,7 +36,53 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('mySecretKeyforConFusion'));
+
+function auth(req, res, next) {
+  console.log(req.signedCookies);
+
+  if(!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+
+    if(!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+
+    var username = auth[0];
+    var password = auth[1];
+
+    if(username === 'admin' && password === 'password') {
+      res.cookie('user', 'admin', { signed: true }); 
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+
+  }
+  else {
+    if(req.signedCookies.user === 'admin') {
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 401;
+      return next(err);
+    }
+  }
+
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
