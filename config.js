b61318fa7625f3b27a4a83ca7fe97fe331856d9f
akshata <wagheakshu@gmail.com>
2019-09-11 16:41:02 +0530

    Facebook passport authentication

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': 'mySecretKeyforAuthentication',
-    'mongoUrl': 'mongodb://akshatamw:akshata2607@ds153730.mlab.com:53730/amsdb'
+    'mongoUrl': 'mongodb://akshatamw:akshata2607@ds153730.mlab.com:53730/amsdb',
+    'facebook': {
+        clientId: '2416425895067249',
+        clientSecret: '84623fc79070985eac0e9bfa20af8bd0'
+    }
 }
+
